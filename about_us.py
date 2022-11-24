95e7b8a5a1f560e6ae2b43ca29e8cc1c36276c3c
PranavRRD19 <92314690+PranavRRD19@users.noreply.github.com>
2022-11-24 18:02:30 +0530

    Updation done
    Done.

--- about_us.py
+++ about_us.py
@@ -2,11 +2,16 @@ from library import *
 from main1 import landing_page
 
 def aboutus():
+    #clears the screen
     os.system("cls")
+    
     print(chalk.red.bold(figlet_format("About Us", font="standard")))
     ENIGMA = colored("ENIGMA", "green", "on_grey")
-    #print("-> Helping Hands is a distributed software developed to maintain the details of employees working in any organization.\n-> This was built by Team", end=" ")
-    print(f"-> Helping Hands is a distributed software developed to maintain the details of employees working in any organization.\n\n-> This was built by Team {ENIGMA} under the guidance of Anand sir.\n\n-> This software has been developed to override the problems prevailing in the practicing manual system.\n\n-> Developed a well designed database to store the information.\n\n-> It aims to help both admin and employees of  a particular organization.\n\n-> The admin can perform various operations such as adding a department,deleting a department etc.\n\n-> It also helps employee to choose their preferred branch and department.\n\n-> We have built a very user friendly application that requires minimal training.")
+    
+    # Tells about the software
+    print(f"-> Helping Hands is a software developed to maintain the details of employees working in the organization.\n\n-> This was built by Team {ENIGMA} under the guidance of Anand sir.\n\n-> This software has been developed to override the problems prevailing in the practicing manual system.\n\n-> Developed a well designed database to store the information.\n\n-> It aims to help both admin and employees of  a particular organization.\n\n-> The admin can perform various operations such as adding a department,deleting a department etc.\n\n-> It also helps employee to choose their preferred branch and department.\n\n-> We have built a very user friendly application that requires minimal training.")
+    
+    # Goes back to landing page on clicking enter
     input("\n\n\tPress Enter to go back.....")
     landing_page()
     

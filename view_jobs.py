54a0b7454643f9e6f3e99ff80e13474660af28df
CHETAN RAJU P M <85154077+chetanrajup@users.noreply.github.com>
2022-11-24 22:54:25 +0530

    Update view_jobs.py

--- view_jobs.py
+++ view_jobs.py
@@ -4,17 +4,25 @@ from admin_home import admin_home
 def helper_view_jobs():
     os.system("cls")
     print(chalk.red.bold.underline("\n\t------------------------------VIEW JOB VACANCIES------------------------------\n"))
+    
+    #connecting to backend database
     mydb = mysql.connector.connect(host="localhost",user="root",database="helping_hands1")
     mycursor = mydb.cursor()    
     l=list()
+    
+    #Fetching job vacancy details
     mycursor.execute("SELECT b.branch_name,b.branch_id, d.dept_name, d.dept_id, d.total_jobs, d.vacant_jobs FROM branch as b inner join department as d on b.branch_id = d.branch_id")
     myresult = mycursor.fetchall()
     for x in myresult:
     	l.append(list(x))
     head = ["Branch name" , "Branch id" , "Deaprtment name" , "Department id" , "Total jobs", "Vacant jobs"]
+    
+    #Displaying the details 
     print(tabulate(l, headers=head, tablefmt="fancy_grid"))
 
 def view_jobs():
     helper_view_jobs()
+    
+    #On click enter event, redirect to home page
     input("\n\nPress Enter to continue")
     admin_home()

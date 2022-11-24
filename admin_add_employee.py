0c403d06d345b55991dc7dd6bb3d0ba97fae85c8
PranavRRD19 <92314690+PranavRRD19@users.noreply.github.com>
2022-11-24 18:53:12 +0530

    Done updating

--- admin_add_employee.py
+++ admin_add_employee.py
@@ -20,19 +20,19 @@ def admin_add_employee():
         #Employee id as an input
         empid=input("\nEnter the employee id : ")
         
-        #Checking if user has already been added
+        #Checking if empid has already been added
         mycursor.execute("SELECT * FROM empid_list where emp_id=%s",(empid,))
         
-        #If user already added, display the error message 
+        #If empid already added, display the error message 
         myresult = mycursor.fetchone()
         if myresult!=None:
             print("An employee with this employee id already exists")
             
-        #If user not added, add employee Id to the database
+        #If empid not added, add employee Id to the database
         elif myresult==None:
             count+=1
             mycursor.execute("insert into empid_list values (%s)",(empid,))
             mydb.commit()
     print(chalk.blue.bold.underline(count),chalk.blue.bold.underline("\nEmployee(s) added"))
     input("Press Enter to continue")
-    admin_home()
+    admin_home() 
